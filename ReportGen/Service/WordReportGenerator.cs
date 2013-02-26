24312219592827895084b46db9192999222673a2
alanwung <alan.yunpeng@gmail.com>
2013-02-26 18:19:18 +0800

    first commit

---
+++ ReportGen/Service/WordReportGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WordDocumentGenerator.Library;
+
+namespace ReportGen.Service
+{
+    public class WordReportGenerator : DocumentGenerator
+    {
+        public WordReportGenerator(DocumentGenerationInfo generationInfo) : base(generationInfo)
+        {
+        }
+
+        protected override Dictionary<string, PlaceHolderType> GetPlaceHolderTagToTypeCollection()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void IgnorePlaceholderFound(string placeholderTag, OpenXmlElementDataContext openXmlElementDataContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void NonRecursivePlaceholderFound(string placeholderTag, OpenXmlElementDataContext openXmlElementDataContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RecursivePlaceholderFound(string placeholderTag, OpenXmlElementDataContext openXmlElementDataContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ContainerPlaceholderFound(string placeholderTag, OpenXmlElementDataContext openXmlElementDataContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
