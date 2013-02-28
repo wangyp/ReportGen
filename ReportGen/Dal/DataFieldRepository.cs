9009ed720da0068eb9131f38c75921d8f5379b8e
alanwung <alan.yunpeng@gmail.com>
2013-02-28 16:27:36 +0800

    Add Dlls

---
+++ ReportGen/Dal/DataFieldRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportGen.Dal
+{
+    public class DataFieldRepository : IDataFieldRepository
+    {
+    }
+}
---
+++ ReportGenTest/DalTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportGen.Dal;
+
+namespace ReportGenTest
+{
+    [TestClass]
+    public class ServiceTest
+    {
+        public IDataFieldRepository DataFieldRepository { get
+        {
+            return new Data
+        }}
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
