ab03f3c3b1226efe92881b9796b7a200d6bde720
alanwung <alan.yunpeng@gmail.com>
2013-02-28 11:39:31 +0800

    add dlls

---
+++ ReportGenTest/ServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReportGenTest
+{
+    [TestClass]
+    public class DalTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
